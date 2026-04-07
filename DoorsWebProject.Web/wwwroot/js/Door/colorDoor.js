b545cb376faf3faa2ff03fc83d69f2eb18faa5df
DudiPower <150533549+DudiPower@users.noreply.github.com>
2026-04-07 17:49:59 +0300

    Add css and js for door colors

---
+++ DoorsWebProject.Web/wwwroot/js/Door/colorDoor.js
@@ -0,0 +1,21 @@
+﻿const colorCircles = document.querySelectorAll('.color');
+const textureLayer = document.getElementById('doorTexture');
+
+colorCircles.forEach(circle => {
+    circle.addEventListener('mouseenter', function () {
+        const texture = this.getAttribute('data-texture-url');
+        const color = this.getAttribute('data-color');
+        console.log(texture);
+        if (texture) {
+            textureLayer.style.backgroundImage = `url(${texture})`;
+        }
+
+        if (color) {
+            textureLayer.style.backgroundColor = color;
+        }
+    });
+
+    circle.addEventListener('mouseleave', function () {
+        textureLayer.style.background = "none"; // връща оригиналната снимка, когато махнеш мишката
+    });
+});
